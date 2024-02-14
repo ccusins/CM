9df2b5e8f18177020ca6b2e2823323d2e1602d0c
ccusins <157019744+ccusins@users.noreply.github.com>
2024-02-14 21:10:45 +0000

    no checkauthentication on bookmaker

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.get('/users', checkAuthentication, (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
 });
 
-app.get('/bookmakers', checkAuthentication, (req, res) => {
+app.get('/bookmakers', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'bookmakers.html'));
 });
 
